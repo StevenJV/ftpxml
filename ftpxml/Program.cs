fb1741f44a538200f93b83ff0a019aaef0b275db
StevenJV <StevenVore@tnwinc.com>
2018-04-17 14:12:40 -0400

    get list of files

---
+++ ftpxml/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ftpxml
+{
+  internal class Program
+  {
+    private static void Main(string[] args){
+      Serverinfo server = new Serverinfo("ftp://aaa.bbb.com", "username", "password");
+      List<string> fileList = GetListOfFiles(server);
+      //DisplayListOfFiles(fileList);
+      //chooseFile();
+      //downloadFile();
+      //parseFile();
+      //writeResults();
+    }
+
+    private static List<string> GetListOfFiles(Serverinfo server){
+      FtpWebRequest request = (FtpWebRequest)WebRequest.Create(server.ServerName);
+      request.Method = WebRequestMethods.Ftp.ListDirectory;
+      request.Credentials = new NetworkCredential(server.Username, server.Password);
+      request.UsePassive = true;
+      request.UseBinary = true;
+      request.KeepAlive = true;
+      FtpWebResponse response = (FtpWebResponse) request.GetResponse();
+      StreamReader reader = new StreamReader(response.GetResponseStream() ?? throw new NullReferenceException());
+      String[] fileNameArray = reader.ReadToEnd().Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+      var fileList = fileNameArray.ToList();
+      return fileList;
+    }
+
+    //private static void DisplayListOfFiles(string fileList)
+    //{
+    //  throw new NotImplementedException();
+    //}
+
+  }
+}
---
+++ ftpxml/Serverinfo.cs
@@ -0,0 +1,18 @@
+﻿namespace ftpxml
+{
+  internal class Serverinfo
+  {
+    public string ServerName { get; set; }
+    //private string _port;
+    public string Username { get; set; }
+    public string Password { get; set; }
+
+    public Serverinfo(string serverName,  string username, string password){
+      ServerName = serverName;
+      //_port = port;
+      Username = username;
+      Password = password;
+    }
+    
+  }
+}
